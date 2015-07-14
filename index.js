bf0c3385d3e049ed1d59ef2e4dda610efe7a3c40
Jason Ibrahim <jibrahim@opentext.com>
2015-07-14 12:18:21 -0700

    updated docs and changed port to 80

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ app.get('/', function (req, res) {
     res.sendFile('index.html');
 });
 
-var server = app.listen(8080, function () {
+var server = app.listen(80, function () {
     var host = server.address().address;
     var port = server.address().port;
 
-    console.log('Example app listening at http://%s:%s', host, port);
+    console.log('Listening at http://%s:%s', host, port);
 });
